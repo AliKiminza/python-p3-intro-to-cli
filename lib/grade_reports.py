f51c3b750f2c259b3fe525d63e85fd556a5fd863
alikiminza <ali.kiminza@student.moringaschool.com>
2023-09-06 16:36:21 +0300

    got CLI working

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+def create_grade_report (student_grades):
+    with open('/home/ali/Desktop/lectures/phase-3/python/python-p3-intro-to-cli/lib/grade_report.txt', 'w') as gr:
+        for grade in student_grades:
+            gr.write(grade+'\n')
 
 if __name__ == '__main__':
-    pass
+    student_grades=[]
+    grade = input("student name, grade:")
+    while grade:
+        student_grades.append(grade)
+
+        grade = input("student name, grade:")
+    create_grade_report(student_grades)
